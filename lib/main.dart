1e6dd6810fc13c5f2925e5caccbc1b065da7e17e
ahmedghaly15 <ahmedghaly0767@gmail.com>
2025-09-13 11:17:48 +0300

    Updated main method

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart' show ProviderScope;
+import 'package:flutter_screenutil/flutter_screenutil.dart' show ScreenUtil;
 
+import 'riverpod_observer.dart';
+import 'src/core/widgets/flutter_error_details_view.dart';
 import 'src/shoppe_app.dart';
 
-void main() {
-  runApp(const ShoppeApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await ScreenUtil.ensureScreenSize();
+
+  ErrorWidget.builder = (FlutterErrorDetails details) =>
+      FlutterErrorDetailsView(details: details);
+
+  runApp(
+    ProviderScope(observers: [RiverpodObserver()], child: const ShoppeApp()),
+  );
 }
