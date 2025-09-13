2e31759ed5679fa36cbe0886f958619a402ba7df
ahmedghaly15 <ahmedghaly0767@gmail.com>
2025-09-13 11:16:38 +0300

    Updated ShoppeApp

---
+++ lib/src/shoppe_app.dart
@@ -0,0 +1,44 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart' show Consumer;
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
+
+import '../app_routes_observer.dart';
+import 'config/router/app_router.dart';
+import 'config/theming/theme_manager.dart';
+import 'core/utils/const_strings.dart';
+
+class ShoppeApp extends StatelessWidget {
+  const ShoppeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, _) => ShadApp.custom(
+        theme: ThemeManager.lightMode,
+        themeMode: ThemeMode.light,
+        appBuilder: (context) => Consumer(
+          builder: (context, ref, child) {
+            final appRouter = ref.read(appRouterProvider);
+            return MaterialApp.router(
+              debugShowCheckedModeBanner: false,
+              title: ConstStrings.appName,
+              theme: Theme.of(context),
+              builder: (_, child) => ShadAppBuilder(child: child),
+              routerConfig: appRouter.config(
+                navigatorObservers: () => [
+                  AppRoutesObserver(),
+                  AutoRouteObserver(),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
