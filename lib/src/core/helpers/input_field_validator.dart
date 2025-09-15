9ea3baf17b5260fe6e3c765a671521026826b021
ahmedghaly15 <ahmedghaly0767@gmail.com>
2025-09-15 22:44:55 +0300

    Added AppRegex & InputFieldValidator

---
+++ lib/src/core/helpers/input_field_validator.dart
@@ -0,0 +1,36 @@
+import 'package:shoppe/src/core/helpers/extensions.dart';
+
+import '../utils/app_regex.dart';
+
+class InputFieldValidator {
+  InputFieldValidator._();
+
+  static String? validatingEmailField(String? value) {
+    if (value.isNullOrEmpty) {
+      return "Email can't be blank!";
+    } else if (!AppRegex.isEmailValid(value!)) {
+      return "Please enter a valid email";
+    }
+    return null;
+  }
+
+  static String? validatingPasswordField(String? value) {
+    if (value.isNullOrEmpty) {
+      return "Password can't be blank!";
+    } else if (!AppRegex.isPasswordValid(value!)) {
+      return "Please enter a valid password";
+    }
+    return null;
+  }
+
+  static String? validatingNameField(String? value) {
+    if (value.isNullOrEmpty) {
+      return "Name can't be blank!";
+    } else if (value!.split(' ').length < 2) {
+      return 'Please enter your last name';
+    } else if (value.length < 2) {
+      return "Name must be at least 2 characters long";
+    }
+    return null;
+  }
+}
