034d80b3c9bcc02363c720b55675cdedd6a87bfa
ahmedghaly15 <ahmedghaly0767@gmail.com>
2025-09-16 10:47:56 +0300

    Finished verify-email feature UI logic

--- lib/src/features/otp/presentation/providers/otp_providers.dart
+++ lib/src/features/otp/presentation/providers/otp_providers.dart
@@ -1,6 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../../../../core/api/api_request_result.dart';
+import '../../data/models/verify_email_request_body.dart';
+import '../../data/repo/verify_email_repo.dart';
+
+part 'otp_providers.g.dart';
 
 final otpProvider = Provider.autoDispose<TextEditingController>(
   (ref) => TextEditingController(),
 );
+
+@riverpod
+class VerifyEmail extends _$VerifyEmail {
+  @override
+  AsyncValue<bool> build() => const AsyncValue.data(false);
+
+  void verify(String email) async {
+    state = const AsyncValue.loading();
+    final requestBody = VerifyEmailRequestBody(
+      email: email,
+      otp: ref.watch(otpProvider).text.trim(),
+    );
+    final result = await ref
+        .read(verifyEmailRepoProvider)
+        .verifyEmail(requestBody);
+    result.when(
+      success: (_) => state = const AsyncValue.data(true),
+      failure: (error) =>
+          state = AsyncValue.error(error.toString(), StackTrace.current),
+    );
+  }
+}
---
+++ lib/src/features/otp/presentation/providers/otp_providers.g.dart
@@ -0,0 +1,63 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'otp_providers.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
+@ProviderFor(VerifyEmail)
+const verifyEmailProvider = VerifyEmailProvider._();
+
+final class VerifyEmailProvider
+    extends $NotifierProvider<VerifyEmail, AsyncValue<bool>> {
+  const VerifyEmailProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'verifyEmailProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$verifyEmailHash();
+
+  @$internal
+  @override
+  VerifyEmail create() => VerifyEmail();
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(AsyncValue<bool> value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<AsyncValue<bool>>(value),
+    );
+  }
+}
+
+String _$verifyEmailHash() => r'cdbc6459cdd98a55869070f3382d6d8bfd59ce50';
+
+abstract class _$VerifyEmail extends $Notifier<AsyncValue<bool>> {
+  AsyncValue<bool> build();
+  @$mustCallSuper
+  @override
+  void runBuild() {
+    final created = build();
+    final ref = this.ref as $Ref<AsyncValue<bool>, AsyncValue<bool>>;
+    final element =
+        ref.element
+            as $ClassProviderElement<
+              AnyNotifier<AsyncValue<bool>, AsyncValue<bool>>,
+              AsyncValue<bool>,
+              Object?,
+              Object?
+            >;
+    element.handleValue(ref, created);
+  }
+}
