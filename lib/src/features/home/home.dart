11085de3045d07d28544680cf4be914a2c86e303
DESKTOP-8H0HA4B\aboghaly <ahmedghaly0767@gmail.com>
2025-09-24 03:55:57 +0300

    Updated HomeView widget

--- lib/src/features/home/home.dart
+++ lib/src/features/home/home.dart
@@ -1,12 +1,29 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shoppe/src/core/helpers/helpers.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
+import '../../config/theming/theming.dart' show AppTextStyles;
 import '../../core/api/api_request_result.dart';
 import '../../core/utils/utils.dart';
+import '../../core/widgets/core_widgets.dart'
+    show
+        CustomCachedNetworkImage,
+        CustomDotIndicator,
+        CustomInputFormField,
+        CustomSkeletonizer;
 import 'data/api/home_api_service.dart';
 import 'data/models/fetch_offers_request_body.dart';
 import 'data/models/fetch_offers_request_response.dart';
 
 part 'presentation/views/home_view.dart';
 part 'data/repo/home_repo.dart';
+part 'presentation/providers/home_providers.dart';
+part 'presentation/views/widgets/home_search_field_consumer.dart';
+part 'presentation/views/widgets/skeletonized_offers_list_view_consumer.dart';
+part 'presentation/providers/fetch_offers_providers.dart';
+part 'presentation/views/widgets/home_section_title.dart';
+part 'presentation/views/widgets/offers_dot_indicators_consumer.dart';
--- lib/src/features/home/presentation/views/home_view.dart
+++ lib/src/features/home/presentation/views/home_view.dart
@@ -6,6 +6,28 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Home View'));
+    return SafeArea(
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 16.h, horizontal: 20.w),
+        child: CustomScrollView(
+          physics: const BouncingScrollPhysics(),
+          slivers: [
+            const SliverToBoxAdapter(child: HomeSearchFieldConsumer()),
+            const SliverToBoxAdapter(
+              child: HomeSectionTitle(titleText: AppStrings.offers),
+            ),
+            const SliverToBoxAdapter(
+              child: SkeletonizedOffersListViewConsumer(),
+            ),
+            SliverToBoxAdapter(
+              child: Container(
+                margin: EdgeInsets.only(top: 10.h, bottom: 19.h),
+                child: const OffersDotIndicatorsConsumer(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
