ee6ce630cfb203033c054bfc2c258036b82be178
ahmedghaly15 <ahmedghaly0767@gmail.com>
2025-09-26 14:53:07 +0300

    Initialized Reviews screen

---
+++ lib/src/features/reviews/presentation/views/reviews_view.dart
@@ -0,0 +1,11 @@
+part of '../../reviews.dart';
+
+@RoutePage()
+class ReviewsView extends StatelessWidget {
+  const ReviewsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text('Reviews')));
+  }
+}
--- lib/src/features/reviews/reviews.dart
+++ lib/src/features/reviews/reviews.dart
@@ -1,3 +1,5 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../core/api/api_request_result.dart';
@@ -8,3 +10,4 @@ import 'data/models/fetch_product_reviews_request_response.dart';
 
 part 'data/repo/reviews_repo.dart';
 part 'presentation/providers/fetch_product_reviews_provider.dart';
+part 'presentation/views/reviews_view.dart';
