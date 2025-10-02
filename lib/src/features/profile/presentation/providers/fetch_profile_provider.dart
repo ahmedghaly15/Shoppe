c256cfffad6c719973d358e16ced886b240359f2
ahmedghaly15 <ahmedghaly0767@gmail.com>
2025-10-02 22:35:55 +0300

    Handled retrieving cached user data

--- lib/src/features/profile/presentation/providers/fetch_profile_provider.dart
+++ lib/src/features/profile/presentation/providers/fetch_profile_provider.dart
@@ -3,6 +3,10 @@ part of '../../profile.dart';
 final fetchProfileProvider = FutureProvider.autoDispose<ShoppeUser>((
   ref,
 ) async {
+  final cachedShoppeUser = await ref
+      .read(secureStorageHelperProvider)
+      .getShoppeUser();
+  if (cachedShoppeUser != null) return cachedShoppeUser;
   final result = await ref.read(profileRepoProvider).fetchProfile();
   return result.when(
     success: (data) => data,
