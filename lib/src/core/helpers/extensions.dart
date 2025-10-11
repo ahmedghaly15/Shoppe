b8c558166c4b8e8e5c2be5a49b0fc6f37941893c
DESKTOP-8H0HA4B\aboghaly <ahmedghaly0767@gmail.com>
2025-10-11 17:32:07 +0300

    Added extension on Iterable  - Added firstWhereOrNull method

--- lib/src/core/helpers/extensions.dart
+++ lib/src/core/helpers/extensions.dart
@@ -116,3 +116,12 @@ extension DioExceptionTypeName on DioException {
 extension Capitalize on String {
   String capitalize() => '${this[0].toUpperCase()}${substring(1)}';
 }
+
+extension IterableExtension<T> on Iterable<T> {
+  T? firstWhereOrNull(bool Function(T element) test) {
+    for (var element in this) {
+      if (test(element)) return element;
+    }
+    return null;
+  }
+}
